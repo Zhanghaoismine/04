222de58d4defd57a8fbe963fba2c5e6675a98fb5
Zhanghaoismine <kegberry@naver.com>
2023-10-04 20:55:38 +0900

    5

--- main.c
+++ main.c
@@ -1,15 +1,18 @@
 #include <stdio.h>
 int main(int argc, char *argv[]) {
-    int a;
-    printf("input a year :");
-    scanf("%i", &a);
-
-    if ((a % 4 == 0 && a % 100 != 0) || (a % 400 == 0)) {
-        printf("%i is a leap year.\n", a);
-    } else {
-        printf("%i is not a leap year.\n", a);
-    }
-
+	unsigned int x;
+	int b;
+	printf("input a number : ");
+	scanf("%ui", &x);
+	for (b=0; x != 0; x >>= 1)
+	{
+		if (x&1)
+		{
+			b++;
+		}
+	}
+	printf("The result is : %i\n", b);
+	
     return 0;
 }
 
